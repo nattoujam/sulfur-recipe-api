afc74e3a3c5e9bfe663f378dd0ed111889ba54d6
nattoujam <Public.kyuuanago@gmail.com>
2025-02-23 16:42:34 +0900

    minor-feat: Recipeの新規作成を追加

--- apps/api/operations/recipe.ts
+++ apps/api/operations/recipe.ts
@@ -1,4 +1,4 @@
-import { PrismaClient } from "@prisma/client";
+import { PrismaClient, Prisma } from "@prisma/client";
 import { Request, Response } from "express";
 import { getLog } from "../utils/log";
 import { toHiragana, toKatakana } from "../utils/japanese";
@@ -6,6 +6,7 @@ import { toHiragana, toKatakana } from "../utils/japanese";
 export const recipeOprations = (prisma: PrismaClient) => {
   return {
     getRecipe: get(prisma),
+    createRecipe: post(prisma),
   };
 };
 
@@ -90,5 +91,35 @@ const get =
       res.json(recipes);
     } catch (e) {
       next(e);
+    }
+  };
+
+const post =
+  (prisma: PrismaClient) => async (req: Request, res: Response, next: any) => {
+    console.log(req.body);
+    try {
+      const data = {
+        resultId: req.body.resultId,
+        amount: req.body.amount,
+        materials: {
+          create: req.body.materials,
+        },
+      };
+      const recipe = await prisma.recipe.create({ data });
+      res.json(recipe);
+    } catch (e: unknown) {
+      console.log(`Failed: ${(e as Error).message}`);
+      if (e instanceof Prisma.PrismaClientKnownRequestError) {
+        console.log(e.code);
+        console.log(e.meta);
+        if (e.code === "P2003") {
+          const message = `Foreign key constraint failed on the field ${e.meta?.field_name}`;
+          res.status(500).json({ code: e.code, message });
+        } else {
+          next(e);
+        }
+      } else {
+        next(e);
+      }
     }
   };
